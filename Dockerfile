c1532265a2648e5f2c52194a59f060a03aabe205
Long Nguyen <long.nguyenviet@lazada.com>
2020-10-30 16:51:58 +0700

    Add more option for nginx config

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM nginx:1.19.2-alpine
+	FROM nginx:1.19.2-alpine
 
 ENV PHPIZE_DEPS \
 		autoconf \
@@ -225,6 +225,7 @@ RUN set -eux; \
 
 # Override stop signal to stop process gracefully
 # https://github.com/php/php-src/blob/17baa87faddc2550def3ae7314236826bc1b1398/sapi/fpm/php-fpm.8.in#L163
+COPY ./nginx.conf /etc/nginx/nginx.conf
 COPY ./conf.d /etc/nginx/conf.d
 COPY ./bin/start.sh /usr/local/bin/start.sh
 RUN chmod +x /usr/local/bin/start.sh
--- bin/start.sh
+++ bin/start.sh
@@ -11,10 +11,12 @@ if [[ -z "${ENV_SERVER_NAME}" ]]; then
 else
   SERVER_NAME="${ENV_SERVER_NAME}"
 fi
-cp /etc/nginx/conf.d/site.conf.tpl /etc/nginx/conf.d/site.conf
+if [ ! -f /etc/nginx/conf.d/site.conf ]; then
+   	cp /etc/nginx/conf.d/site.conf.tpl /etc/nginx/conf.d/site.conf
+	sed -i "s|SERVER_NAME|$SERVER_NAME|g" /etc/nginx/conf.d/site.conf
+	sed -i "s|ROOT_PATH|$ROOT_PATH|g" /etc/nginx/conf.d/site.conf
+fi
 
-sed -i "s|SERVER_NAME|$SERVER_NAME|g" /etc/nginx/conf.d/site.conf
-sed -i "s|ROOT_PATH|$ROOT_PATH|g" /etc/nginx/conf.d/site.conf
 
 if [ -z "$(ls -A ${ROOT_PATH})" ]; then
    echo "<?=phpinfo()?>" > "${ROOT_PATH}/index.php"
